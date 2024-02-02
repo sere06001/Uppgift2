f754d010f511dd710300724b52d62001dffb3696
sere06001 <sere06001@utb.vaxjo.se>
2024-02-02 12:35:06 +0100

    Add project files.

---
+++ Uppgift2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(100);
+            list.Add(4);
+            list.Add(5);
+            Console.WriteLine("Största talet i listan: " + Max(list));
+            Console.ReadKey();
+        }
+        static int Max(List<int> list)
+        {
+            list.Sort();
+            return list[list.Count-1];
+        }
+    }
+}
